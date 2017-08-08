9fbc860f24417bed54a2105a518cda87476ea82d
Tim Engler <engler@gmail.com>
2017-08-08 08:30:20 +0800

    now does recursive synchronization and errors if wait is done outside of lock ownership

--- WhiteBoard/Monitor.hs
+++ WhiteBoard/Monitor.hs
@@ -8,7 +8,7 @@ import Control.Concurrent
 import Control.Monad
 
 data Monitor = Monitor
-    { monitorLock :: MVar ()
+    { monitorLock :: MVar ThreadId
     , monitorCond :: MVar [MVar ()]
     }
 
@@ -29,60 +29,112 @@ whileM cond body = doit
 newMonitor :: IO Monitor
 newMonitor =
   do
-    ml <- newMVar ()
+    ml <- newEmptyMVar
     mc <- newMVar []
     
     return Monitor { monitorLock = ml, monitorCond = mc }
 
 -- | Runs a computation within a monitor.
 synchronized :: Monitor -> IO a -> IO a
-synchronized m doit =
+synchronized m innerProc =
   do
-    --take the monitor lock, so no one else can run, and then run our
-    --IO
-    takeMVar $ monitorLock m
-    v <- doit
-    putMVar (monitorLock m) ()
-    return v
+    --first check if there is a monitor look in our name. If so, we can ignore
+    --it and run doit anyway. (This is how we do recursive sychronization)
+    ownIt <- ownCurrentLock m
+    if ownIt then
+      do
+        --putStrLn "recursive"
+        innerProc
+      else
+      do
+        --create a monitor lock, so no one else can run, and then run our
+        --IO
+        acquireLock m
+        v <- innerProc
+        releaseLock m
+        return v
+
+
+acquireLock :: Monitor -> IO ()
+acquireLock m = myThreadId >>= putMVar (monitorLock m)
+
+releaseLock :: Monitor -> IO ()
+releaseLock m = takeMVar (monitorLock m) >> return ()
+
+
+ownCurrentLock :: Monitor -> IO Bool
+ownCurrentLock m =
+  do
+    myId <- myThreadId
+    mt <- tryReadMVar $ monitorLock m
+    case mt of
+      Just loId -> return $ myId == loId
+      _ -> return False
+
+    
+    
+lockOwner :: Monitor -> IO (Maybe ThreadId)
+lockOwner m = tryReadMVar $ monitorLock m
+
+    
     
 -- | Inside a 'synchronized' block, releases the lock and waits
 -- to be notified
--- WARNING: MUST ONLY BE USED WITHIN A SYNCHRONIZED BLOCK
+-- Must only be used within a synchronization lock, or an error will be thrown
 wait :: Monitor -> IO ()
 wait m =
   do
-    --create a new mvar for us to wait on
-    myC <- newEmptyMVar
+    ownLock <- ownCurrentLock m
+    if not ownLock then
+      do
+        lo <- lockOwner m
+        error ("Can't wait, we don't own the lock! Owned by "++show lo) else
+      do
+    
+        --create a new mvar for us to wait on
+        myC <- newEmptyMVar
 
-    --add that to the monitorCond list. Since we take the monitorCond first
-    --we are guaranteed that no other thread can edit us (because they also take it)
-    mc <- takeMVar $ monitorCond m
-    putMVar (monitorCond m) $ myC : mc
+        --add that to the monitorCond list. Since we take the monitorCond first
+        --we are guaranteed that no other thread can edit us (because they also take it)
+        mc <- takeMVar $ monitorCond m
+        putMVar (monitorCond m) $ myC : mc
 
-    --release the synchronization lock (waits can only be within sychronized blocks
-    putMVar (monitorLock m) ()
+        --release the synchronization lock (waits can only be within sychronized blocks)
+        releaseLock m
 
-    --take our condition, which we set as empty. When a notify occurs, it
-    --will put, which will wake us up so we can continue our thread
-    takeMVar myC
+        --take our condition, which we set as empty. When a notify occurs, it
+        --will put, which will wake us up so we can continue our thread
+        takeMVar myC
 
-    --now that we are done waiting, take the synchronization lock again to continue
-    takeMVar (monitorLock m)
+        --now that we are done waiting, take the synchronization lock again to continue
+        acquireLock m
 
 -- | Notifies the monitor that some conditions may have become true,
 -- and wakes up one process.
-notify :: Monitor -> IO ()
+-- Returns false if no thread woke up in response to notification
+notify :: Monitor -> IO Bool
 notify m =
   do
     mc <- takeMVar $ monitorCond m
-    putStrLn $ "notify: mc length is "++(show $ length mc)
+    --putStrLn $ "notify: mc length is "++(show $ length mc)
     case mc of
-      [] -> putMVar (monitorCond m) mc -- no threads waiting, so just put the monitorCond back
+      [] -> do
+        putMVar (monitorCond m) mc -- no threads waiting, so just put the monitorCond back
+        return False
       myC : mc ->  -- there are thread(s) waiting, so wake one up
         do
           putMVar myC () --wake up the waiting thread
           putMVar (monitorCond m) mc --take the waiting thread off the queue
+          return True
    
+
+-- | Notifies all threads waiting on this monitor
+notifyAll :: Monitor -> IO ()
+notifyAll m =
+  do
+    mc <- takeMVar $ monitorCond m
+    mapM (\myC -> putMVar myC ()) mc
+    return ()
 
 ---------------------------------------------------------------------
 -- Example code:
@@ -108,6 +160,7 @@ newAccount = do
                 curr <- readIORef balance
                 writeIORef balance (curr + n)
                 notify m
+                return ()
             }
 
 makeAccountWithPendingWithdrawal = do
@@ -136,3 +189,9 @@ _test =
     deposit a 20
     deposit a 20
     deposit a 20
+
+_testRecursiveSync =
+  do
+    m <- newMonitor
+    synchronized m $ synchronized m $ putStrLn "hi there"
+
